03dee8ce380e7b36cc2831d62baf7f8bdac63bde
DenisIndenbom <denis.indenbom@yandex.ru>
2026-03-24 14:47:28 +0300

    Changed train augmentation transform

--- utils/transforms.py
+++ utils/transforms.py
@@ -9,14 +9,11 @@ base_transforms = transforms.Compose([
 ])
 
 train_transforms = transforms.Compose([
-    transforms.RandomResizedCrop(224, scale=(0.08, 1.0), ratio=(0.75, 1.33)),
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
     transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomRotation(degrees=15),
-    transforms.ColorJitter(brightness=0.3, contrast=0.3,
-                           saturation=0.2, hue=0.1),
-    transforms.RandomGrayscale(p=0.1),
+    transforms.RandomRotation(degrees=30),
     transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
-
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225]),
